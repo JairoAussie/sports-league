3b8b2b187334f5aab25763082506df02c582a133
jairoAussie <jairoaussie@gmail.com>
2020-12-10 21:19:18 +1000

    add comments to the script

--- run_league.sh
+++ run_league.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#install the necessary gems if you don't have them
 gem install bundle
 bundle install
+#clear the screen to start the app cleaned
 clear
+#run the app with the optional parameter
 ruby index.rb $1
